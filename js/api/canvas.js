0a7fc2a25dce56dd1524566c01f2d9bb8f1ee2b5
Snapper26 <snappertwentysix@gmail.com>
2017-05-26 19:50:52 +1000

    Added spoofing for WebGLRenderingContext.readPixels()

--- js/api/canvas.js
+++ js/api/canvas.js
@@ -12,15 +12,42 @@
     // Real methods from WebGLRenderingContext
     var originalReadPixels = WebGLRenderingContext.prototype.readPixels;
 
-    // TODO: Improve memory usage (particularly for WebGL).
-    // TODO: Is it really necessary to recreate the same data
-    // TODO: in a Uint8Array, Uint8ClampedArray or ImageData Object
-    // TODO: or can we just cut the fat?
-    function fakeCanvas(canvas) {
+    // Real methods from WebGL2RenderingContext
+    var originalReadPixelsTwo = WebGL2RenderingContext.prototype.readPixels;
+
+    // Fake data from an ImageData object
+    function fakeImageData(image) {
         const origin = window.location.hostname;
 
         Math.seedrandom(origin);
 
+        // TODO: Need a faster and more effective spoofing algorithm
+        // TODO: Could the value be cached?
+        for (var i = 0; i < image.data.length; i++) {
+            image.data[i] += (randomBoolean() ? 1 : -1);
+        }
+
+        return image;
+    }
+
+    // Fake data from an ArrayBufferView object
+    function fakePixelData(pixels) {
+        const origin = window.location.hostname;
+
+        Math.seedrandom(origin);
+
+        // TODO: Need a faster and more effective spoofing algorithm
+        // TODO: Could the value be cached?
+        for (var i = 0; i < pixels.length; i++) {
+            pixels[i] += (randomBoolean() ? 1 : -1);
+        }
+
+        return pixels;
+    }
+
+    // Replace the real canvas reference with a fake one
+    function fakeCanvas(canvas) {
+        // First get a reference to the correct context type for "this" canvas
         var ctx = HTMLCanvasElement.prototype.getContext.call(canvas, "2d") ||
             HTMLCanvasElement.prototype.getContext.call(canvas, "webgl") ||
             HTMLCanvasElement.prototype.getContext.call(canvas, "experimental-webgl") ||
@@ -29,12 +56,16 @@
 
         var pixels;
 
+        // Extract the pixel data. Method varies depending on the rendering context's type
         if (ctx instanceof CanvasRenderingContext2D) {
             var image = originalGetImageData.call(ctx, 0, 0, canvas.width, canvas.height);
             pixels = image.data;
         }
         else if (ctx instanceof WebGLRenderingContext || ctx instanceof WebGL2RenderingContext) {
-            // TODO: Fix memory usage here?
+            // TODO: Improve memory usage (particularly for WebGL).
+            // TODO: Is it really necessary to recreate the same data
+            // TODO: in a Uint8Array, Uint8ClampedArray or ImageData Object
+            // TODO: or can we just cut the fat?
             var image = new ImageData(canvas.width, canvas.height);
             var data = new Uint8Array(image.data.length);
             originalReadPixels.call(ctx, 0, 0, canvas.width, canvas.height, ctx.RGBA, ctx.UNSIGNED_BYTE, data);
@@ -47,87 +78,52 @@
             // No methods for pixel data extraction. Nothing to do here ...
         }
 
-        // TODO: Need a faster and more effective spoofing algorithm
-        // TODO: Could the value be cached?
-        for (var i = 0; i < pixels.length; i++) {
-            pixels[i] += (randomBoolean() ? 1 : -1);
-        }
+        // Fake the underlying pixel data
+        var fakePixels = fakePixelData(pixels);
 
+        // Create a deep copy (clone) of the canvas
         var fakeCanvas = canvas.cloneNode(true);
+
+        // Get a 2D context to draw on the fake canvas
         var fakeCtx = fakeCanvas.getContext("2d");
-        console.log("Fake Context Null? " + (fakeCtx === null));
-        var image = new ImageData(pixels, canvas.width, canvas.height);
+
+        // Build a new ImageData object using the fake pixel data
+        var image = new ImageData(fakePixels, canvas.width, canvas.height);
+
+        // Obscure the canvas with fake pixel data
         fakeCtx.putImageData(image, 0, 0);
+
         return fakeCanvas;
     }
 
+    // CanvasRenderingContext2D specific method for extracting image data from a canvas.
     CanvasRenderingContext2D.prototype.getImageData = function(sx, sy, sw, sh) {
         console.log("[ALERT] " + window.location.hostname + " called CanvasRenderingContext2D.getImageData()");
 
-        const origin = window.location.hostname;
-
-        Math.seedrandom(origin);
-
         var image = originalGetImageData.call(this, sx, sy, sw, sh);
 
-        return randomiseImageData(image);
+        return fakeImageData(image);
     };
 
-/*
+    // WebGLRenderingContext specific method for extracting pixel data from a canvas.
     WebGLRenderingContext.prototype.readPixels = function(x, y, width, height, format, type, pixels) {
         console.log("[ALERT] " + window.location.hostname + " called WebGLRenderingContext.readPixels()");
 
-        const origin = window.location.hostname;
-
-        Math.seedrandom(origin);
-
         originalReadPixels.call(this, x, y, width, height, format, type, pixels);
 
-        if (format === WebGLRenderingContext.ALPHA) {
-            if (type === WebGLRenderingContext.UNSIGNED_BYTE) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_5_6_5) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_4_4_4_4) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_5_5_5_1) {
-
-            }
-        }
-        else if (format === WebGLRenderingContext.RGB) {
-            if (type === WebGLRenderingContext.UNSIGNED_BYTE) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_5_6_5) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_4_4_4_4) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_5_5_5_1) {
-
-            }
-        }
-        else if (format === WebGLRenderingContext.RGBA) {
-            if (type === WebGLRenderingContext.UNSIGNED_BYTE) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_5_6_5) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_4_4_4_4) {
-
-            }
-            else if (type === WebGLRenderingContext.UNSIGNED_SHORT_5_5_5_1) {
-
-            }
-        }
+        fakePixelData(pixels);
     };
-*/
 
+    // WebGL2RenderingContext specific method for extracting pixel data from a canvas.
+    WebGL2RenderingContext.prototype.readPixels = function(x, y, width, height, format, type, pixels) {
+        console.log("[ALERT] " + window.location.hostname + " called WebGL2RenderingContext.readPixels()");
+
+        originalReadPixelsTwo.call(this, x, y, width, height, format, type, pixels);
+
+        fakePixelData(pixels);
+    };
+
+    // HTMLCanvasElement methods. These work on a HTMLCanvasElement object.
     HTMLCanvasElement.prototype.toDataURL = function(type, encoderOptions) {
         console.log("[ALERT] " + window.location.hostname + " called HTMLCanvasElement.toDataURL()");
 
